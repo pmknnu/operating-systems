9ed98512106af0f04c623645ca52d6fa2c3a1e18
sridhar.gude <sridhar.gude@sjsu.edu>
2016-05-15 20:09:43 +0000

    word count program is done

--- hw0/wc.c
+++ hw0/wc.c
@@ -1,3 +1,44 @@
+#include<stdio.h>
+
 int main(int argc, char *argv[]) {
+    if (argc<2){
+      //Checking for file argument given
+    printf("Provide file\n");
+  }
+    else{
+    printf("Reading from %s\n", argv[1]);
+  }
+
+  FILE *fp;
+
+  fp=fopen(argv[1],"r");
+
+  if(fp==NULL){
+    fprintf(stderr, "Can't open file: %s\n",argv[1] );
+    exit(1);
+  }
+
+  int count=0,lines=0;
+  char k;
+  while(1){
+    k=getc(fp);
+    if(k=='\n'){     //checking new line
+      lines+=1;
+      count+=1;
+    }
+    else if(feof(fp)){   //check for End Of File
+      break;
+    }
+    else if(k!='\r'){    //check for /r tab
+      count+=1;
+    }
+    else{
+      count+=1;
+    }
+
+  }
+  printf("Count: %d\n",count );
+  printf("Lines:%d\n",lines);
+
     return 0;
 }
